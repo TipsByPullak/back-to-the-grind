04e932195523f28879190a95f927c98e685577a9
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2026-08-06 02:22:46 +0530

    Add: singlyLinkedList - submission-8

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-8.cpp
@@ -0,0 +1,136 @@
+class Node {
+public:
+    int val = 0;
+    Node *next = NULL;
+};
+
+class LinkedList {
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int size = 0;
+
+    LinkedList() {
+
+    }
+
+    int get(int index) {
+        if (index >= size) return -1;
+
+        // Traversal
+        if (head == NULL) return -1;
+        if ((index + 1) == size) return tail->val;
+        if (index == 0) return head->val;
+
+        Node *nd = NULL;
+        nd = head;
+
+        int i = 0;
+        while (i < index)
+        {
+            nd = nd->next;
+            ++i;
+        }
+
+        return nd->val;
+    }
+
+    void insertHead(int val) {
+        Node *newNode = new Node;
+        newNode->val = val;
+
+        if (size == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+        
+        size++;
+    }
+    
+    void insertTail(int val) {
+        Node *newNode = new Node;
+        newNode->val = val;
+
+        if (size == 0)
+        {
+            tail = newNode;
+            head = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        ++size;
+    }
+
+    bool remove(int index) {
+        if (index >= size)
+        {
+            return false;
+        }
+
+        if (index == 0)
+        {
+            Node *nd = head;
+            head = head->next;
+
+            delete nd;
+            --size;
+            return true;
+        }
+        else 
+        {
+            Node *prev = head;
+            Node *curr = head->next;
+            Node *next = curr->next;
+
+            int i = 1;
+            
+            while (i != index)
+            {
+                if (i > 1)
+                    prev = prev->next;
+                
+                next = next->next;
+                curr = curr->next;
+
+                ++i;
+            }
+
+            if (curr == tail)
+            {
+                tail = prev;
+            }
+
+            prev->next = next;
+
+            delete(curr);
+            --size;
+            return true;
+        }
+    }
+
+    vector<int> getValues() {
+        vector<int> v(size);
+
+        Node *tr = head;
+        for (int i = 0; i < v.size(); ++i)
+        {
+            if (tr != NULL)
+            {
+                v[i] = tr->val;
+                tr = tr->next;
+            }
+        }
+
+        return v;
+    }
+};
