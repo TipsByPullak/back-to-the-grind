455f696801e69a402a03140e821f026e8c4a42aa
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2026-09-10 21:55:37 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,40 @@
+class MinStack {
+public:
+    stack<int> elements;
+    stack<int> minElements;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        elements.push(val);
+
+        if (minElements.empty() || val <= minElements.top())
+        {
+            minElements.push(val);
+        }
+    }
+    
+    void pop() {
+        int element = elements.top();
+        elements.pop();
+
+        int minElement = minElements.top();
+        if (element == minElement) minElements.pop();
+    }
+    
+    int top() {
+        if (!elements.empty())
+            return elements.top();
+
+        return -1;
+    }
+    
+    int getMin() {
+        cout << minElements.top() << endl;
+        if (!minElements.empty())
+            return minElements.top();
+
+        return -1;
+    }
+};
