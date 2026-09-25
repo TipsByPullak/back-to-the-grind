5d58fbad0c12629cf90a4d7573a21bca8d356e8e
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2026-09-26 02:25:42 +0530

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.cpp
@@ -0,0 +1,57 @@
+class Brick {
+    public:
+        int height;
+        int index;
+};
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = 0;
+
+        stack<Brick> stk;
+
+        for(int i = 0; i < heights.size(); ++i)
+        {
+            if (stk.empty() || stk.top().height < heights[i])
+            {
+                Brick b;
+                b.height = heights[i];
+                b.index = i;
+
+                stk.push(b);
+            }
+            else if (stk.top().height > heights[i])
+            {
+                Brick currB; currB.height = heights[i]; currB.index = i;
+
+                while(!stk.empty() && (stk.top().height > heights[i]))
+                {
+                    Brick topB = stk.top();
+
+                    maxArea = max(abs(topB.height * (currB.index - topB.index)), maxArea);
+
+                    cout << "max area- " << maxArea << endl;
+
+                    currB.index = topB.index;
+
+                    stk.pop();
+                }
+
+                stk.push(currB);
+            }
+        }
+
+        while(!stk.empty())
+        {
+            Brick topB = stk.top();
+
+            maxArea = max(abs(topB.height * ((int)heights.size() - topB.index)), maxArea);
+            cout << "max area- " << maxArea << endl;
+
+            stk.pop();
+        }
+
+        return maxArea;
+    }
+};
