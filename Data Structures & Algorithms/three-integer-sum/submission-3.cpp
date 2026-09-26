dafc2ea9a293c440002e6f0a97129bb8486841fc
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2026-09-27 00:15:01 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    void getResult(vector<vector<int>> &result, vector<int> &search, int target)
+    {
+        int l = 0, r = search.size() - 1;
+
+        while(l < r)
+        {
+            if ((search[l] + search[r]) > target) r--;
+            else if ((search[l] + search[r]) < target) l++;
+            else
+            {
+                result.push_back({-1 * target, search[l], search[r]});
+
+                while (l < r && search[l] == search[l + 1]) l++;
+                while (l < r && search[r] == search[r - 1]) r--;
+
+                l++;
+                r--;
+            }
+        }
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+
+        if (nums.size() == 3)
+        {
+            if (nums[0] + nums[1] + nums[2] == 0)
+            {
+                result.push_back(nums); 
+            }
+
+            return result;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size() - 2; ++i)
+        {
+            if (nums[i] > 0) break;
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            vector<int> search(nums.begin() + i + 1, nums.end());
+
+            getResult(result, search, -1 * nums[i]);
+        }
+
+        return result;
+    }
+};
